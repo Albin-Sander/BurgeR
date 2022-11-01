d63490797cd9d5e23d2360da29493ac1db5e12dc
Albin Sander <51294735+Albin-Sander@users.noreply.github.com>
2022-11-01 21:54:46 +0100

    Initial Commit

---
+++ BurgeR/BurgeRApp.swift
@@ -0,0 +1,17 @@
+//
+//  BurgeRApp.swift
+//  BurgeR
+//
+//  Created by Albin Sander on 2022-11-01.
+//
+
+import SwiftUI
+
+@main
+struct BurgeRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
