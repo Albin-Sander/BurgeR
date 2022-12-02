c34e362f38ea769bf0ec30818a37e5628abc11fd
Albin Sander <51294735+Albin-Sander@users.noreply.github.com>
2022-12-02 23:54:57 +0100

    New UI color and navigationview

--- views/AddNewItemView.swift
+++ views/AddNewItemView.swift
@@ -5,8 +5,8 @@
 //  Created by Albin Sander on 2022-11-06.
 //
 
-import SwiftUI
 import CloudKit
+import SwiftUI
 
 struct AddNewItemView: View {
     @State var name: String
@@ -15,7 +15,7 @@ struct AddNewItemView: View {
     @State var author: String
     @State var image: Data?
     @StateObject private var vm: ItemListViewModel
-    
+
     init(vm: ItemListViewModel) {
         _vm = StateObject(wrappedValue: vm)
         vm.populateItems()
@@ -24,67 +24,76 @@ struct AddNewItemView: View {
         description = ""
         author = ""
     }
+
     var body: some View {
-        Form {
-            VStack {
-                Spacer()
-                Section(header: Text("Enter the name of the burger resturant")
-                    .font(.system(size: 15))
-                ) {
-                    TextField("Enter name", text: $name)
-                        .textFieldStyle(.roundedBorder)
-                }
-            
-                PhotoPickerView(selectedImageData: $image)
-                    .padding(.bottom, 20)
-                    .padding(.top, 20)
-                Section(header: Text("Describe your experience")
-                    .font(.system(size: 15))
-                ) {
-                    TextField("Description", text: $description)
-                        .textFieldStyle(.roundedBorder)
-                }
-                Spacer()
-                Section(header: Text("How many stars would you rate the burgers?")
-                    .font(.system(size: 15))
-                ) {
-                    TextField("How many stars would you rate the burgers?", value: $stars, formatter: NumberFormatter())
-                        .textFieldStyle(.roundedBorder)
-                }
-                Spacer()
-                Section(header: Text("Written by")
-                    .font(.system(size: 15))
-                ) {
-                    TextField("Author", text: $author)
-                        .textFieldStyle(.roundedBorder)
-                }
-                Spacer()
-                Button {
-                    vm.saveItem(name: name, stars: stars, description: description, author: author, image: image!)
+        NavigationView {
+            Form {
+               
+                
+                
                     
-                    self.name = ""
-                    self.stars = 1
-                    self.description = ""
-                    self.author = ""
-                } label: {
-                    Text("Save")
+                    Section(header:
+                                VStack(alignment: .leading) {
+                        Text("Name of resturant")
+                            .font(.system(size: 15))
+                    }
+                    ) {
+                        TextField("Enter name", text: $name)
+                        PhotoPickerView(selectedImageData: $image)
+                            .padding(.bottom, 20)
+                            .padding(.top, 20)
+                        
+                    }
+
+                   
+                    Section(header: Text("Describe your experience")
+                        .font(.system(size: 15))
+                    ) {
+                        TextField("Description", text: $description)
+                     
+                    }
+                    Section(header: Text("Rating (1-5)")
+                        .font(.system(size: 15))
+                    ) {
+                        TextField("How many stars would you rate the burgers?", value: $stars, formatter: NumberFormatter())
+                    }
+                    Section(header: Text("Written by")
+                        .font(.system(size: 15))
+                    ) {
+                        TextField("Author", text: $author)
+                    }
+                    Button {
+                        vm.saveItem(name: name, stars: stars, description: description, author: author, image: image!)
+
+                        self.name = ""
+                        self.stars = 1
+                        self.description = ""
+                        self.author = ""
+                    } label: {
+                        Text("Save")
+                            .padding(5)
+                    }
+                    .background(Color.purple)
+                    .buttonStyle(.bordered)
+                    .cornerRadius(5)
+                    .foregroundColor(Color.white)
+                    .disabled(name.isEmpty || description.isEmpty || author.isEmpty)
                 }
-                .background(Color.purple)
-                .buttonStyle(.bordered)
-                .foregroundColor(Color.white)
-                .disabled(name.isEmpty || description.isEmpty || author.isEmpty)
-                
-                
-                
-                
-            }
             .padding()
+            .navigationBarTitle("Add Review")
+            .background(Color.gray.opacity(0.07))
         }
+        .scrollContentBackground(.hidden)
+
+       
+        }
+    
     }
-}
+
+
 
 struct AddNewItemView_Previews: PreviewProvider {
     static var previews: some View {
-       AddNewItemView(vm: ItemListViewModel(container: CKContainer.default()))
+        AddNewItemView(vm: ItemListViewModel(container: CKContainer.default()))
     }
 }
