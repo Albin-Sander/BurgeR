8f599d54c3f6c51c87c38fbb90e87929282aeaa0
Albin Sander <51294735+Albin-Sander@users.noreply.github.com>
2022-12-03 00:03:57 +0100

    Removed background from button

--- views/AddNewItemView.swift
+++ views/AddNewItemView.swift
@@ -28,43 +28,46 @@ struct AddNewItemView: View {
     var body: some View {
         NavigationView {
             Form {
-               
                 
                 
+                
+                
+                Section(header:
+                            VStack(alignment: .leading) {
+                    Text("Name of resturant")
+                        .font(.system(size: 15))
+                }
+                ) {
+                    TextField("Enter name", text: $name)
+                    PhotoPickerView(selectedImageData: $image)
+                        .padding(.bottom, 20)
+                        .padding(.top, 20)
                     
-                    Section(header:
-                                VStack(alignment: .leading) {
-                        Text("Name of resturant")
-                            .font(.system(size: 15))
-                    }
-                    ) {
-                        TextField("Enter name", text: $name)
-                        PhotoPickerView(selectedImageData: $image)
-                            .padding(.bottom, 20)
-                            .padding(.top, 20)
-                        
-                    }
-
-                   
-                    Section(header: Text("Describe your experience")
-                        .font(.system(size: 15))
-                    ) {
-                        TextField("Description", text: $description)
-                     
-                    }
-                    Section(header: Text("Rating (1-5)")
-                        .font(.system(size: 15))
-                    ) {
-                        TextField("How many stars would you rate the burgers?", value: $stars, formatter: NumberFormatter())
-                    }
-                    Section(header: Text("Written by")
-                        .font(.system(size: 15))
-                    ) {
-                        TextField("Author", text: $author)
-                    }
+                }
+                
+                
+                Section(header: Text("Describe your experience")
+                    .font(.system(size: 15))
+                ) {
+                    TextField("Description", text: $description)
+                    
+                }
+                Section(header: Text("Rating (1-5)")
+                    .font(.system(size: 15))
+                ) {
+                    TextField("How many stars would you rate the burgers?", value: $stars, formatter: NumberFormatter())
+                }
+                
+                Section(header: Text("Written by")
+                    .font(.system(size: 15))
+                ) {
+                    TextField("Author", text: $author)
+                }
+                
+                VStack() {
                     Button {
                         vm.saveItem(name: name, stars: stars, description: description, author: author, image: image!)
-
+                        
                         self.name = ""
                         self.stars = 1
                         self.description = ""
@@ -73,16 +76,20 @@ struct AddNewItemView: View {
                         Text("Save")
                             .padding(5)
                     }
+                   
                     .background(Color.purple)
                     .buttonStyle(.bordered)
                     .cornerRadius(5)
                     .foregroundColor(Color.white)
                     .disabled(name.isEmpty || description.isEmpty || author.isEmpty)
                 }
+                .listRowBackground(Color.clear)
+            }
             .padding()
             .navigationBarTitle("Add Review")
             .background(Color.gray.opacity(0.07))
         }
+        
         .scrollContentBackground(.hidden)
 
        
